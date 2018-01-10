a8dfb03ac6df449ade1591cb7fcf8e9bbf805ef2
Priyanka Malviya <priyankamalviya@Priyankas-MacBook-Pro.local>
2018-01-09 20:55:25 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Reeplay
+
+Search and play Youtube videos faster!! Uses React, lodash and youtube-api-search.
+
+
+#### How to use this app?
+
+
+```
+> git clone https://github.com/StephenGrider/ReduxSimpleStarter.git
+> cd ReduxSimpleStarter
+> npm install
+> npm start
+```
---
+++ src/index.js
@@ -0,0 +1,66 @@
+import _ from 'lodash';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_details';
+
+const API_KEY ='AIzaSyB1d7njORVLD6S0Ye3Al-MO3BAba7uMcLs';
+
+class App extends Component {
+  constructor(props){
+    super(props);
+
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    }; //default your state here
+
+    YTSearch({
+      key: API_KEY,
+      term: 'surfboards'
+    	},
+			(videos) => {
+        // console.log(data);
+        this.setState({
+          videos: videos,
+           selectedVideo: videos[0]
+        });
+    	}
+		);
+
+		this.videoSearch('fireworks'); // initialize the search term to a default value
+  }
+
+	videoSearch(term) {
+		YTSearch({
+			key: API_KEY,
+			term: term
+			},
+			(videos) => {
+				// console.log(data);
+				this.setState({
+					videos: videos,
+					 selectedVideo: videos[0]
+				});
+			}
+		);
+	}
+  render(){
+
+		const videoSearch = _.debounce(term => {this.videoSearch(term)}, 300);
+      return (
+        <div>
+            <SearchBar onSearchChange = {videoSearch} />
+            <VideoDetail video={this.state.selectedVideo}/>
+            <VideoList
+            	onVideoSelect = {selectedVideo => this.setState({selectedVideo})}
+            	videos={this.state.videos} />
+        </div>
+      );
+  }
+}
+
+// React - plz take this component and put it on the page
+ReactDOM.render(<App />, document.querySelector(".container"));  // App is a class, <App /> is an instance of a class
